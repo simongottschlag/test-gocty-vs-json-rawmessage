6d457215e996f25fe44e96d681d39411efe57292
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-12-09 21:52:46 +0100

    add test case for example

--- pkg_test.go
+++ pkg_test.go
@@ -49,6 +49,46 @@ func TestCheck(t *testing.T) {
 	}
 }
 
+func TestCheckExample(t *testing.T) {
+	want := Map{
+		"arr_fail": b(`[{"x":[null]}]`),
+		"arr_ok":   b(`[1.0,["\/",{"x":[]}]]`),
+		"f_fail":   b(`false`),
+		"f_ok":     b(`false`),
+		"n_fail":   b(`null`),
+		"n_ok":     b(`null`),
+		"num_fail": b(`5.1`),
+		"num_ok":   b(`5.3e1`),
+		"obj_fail": b(`{"missing":[],"fail":["\/",null]}`),
+		"obj_ok":   b(`{"ok":[]}`),
+		"str_fail": b(`"pqr"`),
+		"str_ok":   b(`"\/"`), // json forward slashes can be optionally escaped
+		"t_fail":   b(`true`),
+		"t_ok":     b(`true`),
+	}
+
+	got := Map{
+		"arr_fail": b(`[{"x":[[]]},true]`),
+		"arr_ok":   b(`[10e-1,["/",{"x":["extra"]}]]`),
+		"f_fail":   b(`true`),
+		"f_ok":     b(`false`),
+		"n_fail":   b(`false`),
+		"n_ok":     b(`null`),
+		"num_fail": b(`5e1`),
+		"num_ok":   b(`53`), // different format of same value
+		"obj_fail": b(`{"fail":[0]}`),
+		"obj_ok":   b(`{"ok":["extra"],"bonus":"field"}`),
+		"str_fail": b(`"xyz"`),
+		"str_ok":   b(`"/"`), // different format of same value
+		"t_fail":   b(`100`),
+		"t_ok":     b(`true`),
+		"extra":    b(`"doesn't matter"`),
+	}
+
+	msgs := Check(got, want)
+	require.Len(t, msgs, 9)
+}
+
 func testToTestRawCases(tb testing.TB, cases []testCase) []testRawCase {
 	tb.Helper()
 
